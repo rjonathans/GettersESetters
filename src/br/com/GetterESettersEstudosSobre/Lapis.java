7162cd29f456cb36dc45d50926e0405a1d3de548
rjonathans <49173110+rjonathans@users.noreply.github.com>
2019-04-08 00:58:21 -0300

    Criando a Classe Cliente com seus Get, Set e construtores.

--- src/br/com/GetterESettersEstudosSobre/Lapis.java
+++ src/br/com/GetterESettersEstudosSobre/Lapis.java
@@ -3,12 +3,12 @@ package br.com.GetterESettersEstudosSobre;
 import com.sun.media.sound.ModelOscillator;
 
 public class Lapis {
-    public String modelo;
     private float ponta;
-    boolean tampada;
-    public String cor;
-    public boolean apontado;
-    
+    private boolean tampada;
+    private String cor;
+    private boolean apontado;
+    private String modelo;
+
     //Metodo
     public void status(){
         System.out.println("Eu tenho um Lapis");
@@ -24,40 +24,40 @@ public class Lapis {
         return modelo;
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
+    public void setModelo(String m) {
+        this.modelo = m;
     }
 
     public float getPonta() {
         return ponta;
     }
 
-    public void setPonta(float ponta) {
-        this.ponta = ponta;
+    public void setPonta(float p) {
+        this.ponta = p;
     }
 
     public boolean isTampada() {
         return tampada;
     }
 
-    public void setTampada(boolean tampada) {
-        this.tampada = tampada;
+    public void setTampada(boolean t) {
+        this.tampada = t;
     }
 
     public String getCor() {
         return cor;
     }
 
-    public void setCor(String cor) {
-        this.cor = cor;
+    public void setCor(String c) {
+        this.cor = c;
     }
 
     public boolean isApontado() {
         return apontado;
     }
 
-    public void setApontado(boolean apontado) {
-        this.apontado = apontado;
+    public void setApontado(boolean a) {
+        this.apontado = a;
     }
 
 }
